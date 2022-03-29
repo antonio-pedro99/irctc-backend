3c4c409f33090ac8b46920e32dda062f5bb18ee1
Antonio Pedro <tonio.pedro99@gmail.com>
2022-03-29 19:40:14 +0530

    fix imports

--- routes/passengers.py
+++ routes/passengers.py
@@ -1,8 +1,50 @@
+from lib2to3.pgen2.token import OP
+from typing import List, Optional
 from fastapi import APIRouter
 from config import db
+import sqlalchemy
+from schemas.passenger import *
 
 passenger_route = APIRouter()
 
-@passenger_route.get("/passengers")
+@passenger_route.get("/passengers",  response_model=List[Passenger])
 def get_passengers():
     return db.engine.execute("select * from passengers").all()
+
+
+@passenger_route.get("/trips")
+def get_trips(location_from:str, location_to:Optional[str]):
+
+    #run raw sql queries with python variables
+    query = sqlalchemy.text("""
+        SELECT
+            *
+        FROM
+            trips AS T
+        WHERE
+            T.routeID IN (SELECT
+                    routeID
+                FROM
+                    routes AS R
+                WHERE
+                    R.location IN (SELECT
+                            id
+                        FROM
+                            stations AS S
+                        WHERE
+                            S.location = '{0}')
+                        AND R.final_destination IN (SELECT
+                            id
+                        FROM
+                            stations AS S
+                        WHERE
+                            S.location = '{1}'))
+                AND T.train_id IN (SELECT
+                    train_id
+                FROM
+                    seats AS S
+                WHERE
+                    S.s_status = 0);
+                    
+    """.format(location_from, location_to))
+    return db.engine.execute(query).all()
---
+++ schemas/passenger.py
@@ -0,0 +1,13 @@
+import email
+from pydantic import BaseModel
+from typing import Optional
+
+class Passenger(BaseModel):
+    name:str
+    email:str
+    phone:str
+    age:int
+
+
+class PassengerCreate(Passenger):
+    id:int
