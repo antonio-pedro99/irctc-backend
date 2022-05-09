2e44c652d53ca357ce5b94071f0ae5a9147e428f
Antonio Pedro <tonio.pedro99@gmail.com>
2022-05-09 15:46:52 +0530

    done

--- config/security.py
+++ config/security.py
@@ -3,14 +3,7 @@ from passlib.context import CryptContext
 key = Fernet.generate_key()
 fer = Fernet(key)
 
-
 pwd_context = CryptContext(deprecated="auto", schemes=["bcrypt"])
-""" def hash_password(password:str):
-    return fer.encrypt(password.encode("utf-8"))
-
-def unhash_password(hashed_password):
-    return fer.decrypt(token=hashed_password).decode("utf-8") """
-
 
 def verify_pwd(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
