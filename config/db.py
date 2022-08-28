46f691cd78e42726ec827d5a90fabf838fc7a7ca
pedro <tonio.pedro99@gmail.com>
2022-08-28 18:48:57 +0530

    fix the db url

--- config/db.py
+++ config/db.py
@@ -3,7 +3,7 @@ import databases
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL = "mysql+pymysql://admin:root1234@irctc-db-id.cix1p67429p1.us-east-2.rds.amazonaws.com:3306/irctc_db"
+DATABASE_URL = "mysql+pymysql://admin:root1234@3.17.218.216:3306/irctc_db"
 
 database = databases.Database(DATABASE_URL)
 
